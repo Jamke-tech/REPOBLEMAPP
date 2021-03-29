2222160c1d2ce13bbc233b3f626c283089840b03
Jamke-tech <jaume_taber10@hotmail.com>
2021-03-29 19:31:35 +0200

    Preparació backend

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,23 @@
+import {Router} from 'express';
+import multer from '../libs/multer'
+
+//Construim el router on posarem totes les entrades de la API
+const router = Router();
+
+//RECORDATORI LES RUTES DE LA API COMENÇEN PER /API
+
+
+/* Exemple de entrada de api
+router.route('/')
+    .get ( Funció que ha de fer)*/
+
+//IMPORTAR LES FUNCIONS DELS CONTROLLERS
+
+
+
+
+//Exportem totes les rutes per que les utilitzin
+
+export default router;
+
+
