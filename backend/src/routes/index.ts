cb1e5c9aab0e6f01c3ccfa6797ff42ee1a026496
Juan Antonio Cardenas <juan.antonio.cardenas@estudiantat.upc.edu>
2021-04-04 01:18:36 +0200

    Primeres functions i routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,16 @@
-import {Router} from 'express';
-import multer from '../libs/multer'
+import { Router } from "express";
+import {
+  createUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.controller";
+import multer from "../libs/multer";
 
 //Construim el router on posarem totes les entrades de la API
 const router = Router();
 
 //RECORDATORI LES RUTES DE LA API COMENÇEN PER /API
-
 
 /* Exemple de entrada de api
 router.route('/')
@@ -13,11 +18,9 @@ router.route('/')
 
 //IMPORTAR LES FUNCIONS DELS CONTROLLERS
 
-
-
+router.route("/user").get(getUsers).post(createUser);
+router.route("/user/:id").put(updateUser).get(getUser);
 
 //Exportem totes les rutes per que les utilitzin
 
 export default router;
-
-
