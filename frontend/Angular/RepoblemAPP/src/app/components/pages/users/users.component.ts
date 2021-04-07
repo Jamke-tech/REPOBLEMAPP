be674aed56296c478b6165d6f41a69b97c8503cb
AbelOrPa <abel.ortiz@estudiantat.upc.edu>
2021-04-07 10:14:17 +0200

    Añado html de los diferentes componentes y algo mas

--- frontend/Angular/RepoblemAPP/src/app/components/pages/users/users.component.ts
+++ frontend/Angular/RepoblemAPP/src/app/components/pages/users/users.component.ts
@@ -6,7 +6,7 @@ import { User } from '../../../shared/models/user.interface';
     selector:'app-users',
     template:`
     <div class="dsc-card" *ngIf="user">
-        <a [routerLink]="['/user-details']">
+        <a [routerLink]="['/users-details']">
             <div class="dsc-card__img">
                 <img [src]="user.avatar" alt="">
             </div>
@@ -15,6 +15,13 @@ import { User } from '../../../shared/models/user.interface';
                 <p>Correu: {{ user.email }}</p>
             </div>
         </a>
+        <div>
+            <button class="btn btn-danger btn-remove btn-sm" style="margin-left: 2px;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                </svg>
+            </button>
+        </div>
     </div>`,
     changeDetection:ChangeDetectionStrategy.OnPush
 })
