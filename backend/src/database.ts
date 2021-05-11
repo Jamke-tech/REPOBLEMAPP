a73ce6cd7d76e22e6a0d42af51c839ec22911f22
Juan Antonio Cardenas <juan.antonio.cardenas@estudiantat.upc.edu>
2021-05-11 21:07:06 +0200

    canvis per producció

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -4,7 +4,7 @@ import { isJSDocAugmentsTag } from "typescript";
 export async function startConnection() {
   //Accedim a la BBDD que tenim ( en dev estarà en local )
   //Canviar el .connect (mongo o 127.0.0.1)!
-  const db = await mongoose.connect("mongodb://127.0.0.1:27017/REPOBLEMAPP", {
+  const db = await mongoose.connect("mongodb://mongo:27017/REPOBLEMAPP", {
     useNewUrlParser: true,
     useFindAndModify: false,
   });
